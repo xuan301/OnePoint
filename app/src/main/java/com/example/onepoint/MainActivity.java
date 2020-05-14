4b3512e7c6d36b2cf7c28cf116197407afec8f50
wulala2580 <635033547@qq.com>
2020-05-14 18:55:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/onepoint/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.onepoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*getSupportActionBar().hide();
+        /*if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }*/
+        setContentView(R.layout.activity_main);
+
+        Button button_randomknowledge = (Button) findViewById(R.id.randomknowledge);
+        button_randomknowledge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RandomKnowledgeActivity.class);
+                startActivity(intent);
+            }
+        })
+        ;
+
+        Button button_read = (Button) findViewById(R.id.read);
+        button_read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ReadActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button_like = (Button) findViewById(R.id.like);
+        button_like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LikeActivity.class);
+                startActivity(intent);
+            }
+        })
+        ;
+
+        Button button_comment = (Button) findViewById(R.id.comment);
+        button_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CommentActivity.class);
+                startActivity(intent);
+            }
+        })
+        ;
+
+        Button button_search = (Button) findViewById(R.id.search);
+        button_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        })
+        ;
+
+        Button button_rank = (Button) findViewById(R.id.rank);
+        button_rank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RankActivity.class);
+                startActivity(intent);
+            }
+        })
+        ;
+
+        Button button_setting = (Button) findViewById(R.id.setting);
+        button_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                startActivity(intent);
+            }
+        })
+        ;
+    }
+
+
+
+}
