98962e67e4751fe8b67f32fa208ea2bc4edfe143
ji-zhe <43602150+ji-zhe@users.noreply.github.com>
2020-05-15 21:56:31 +0800

    add like activity demo

---
+++ app/src/main/java/com/example/onepoint/Knowledge.java
@@ -0,0 +1,4 @@
+package com.example.onepoint;
+
+public class Knowledge {
+}
---
+++ app/src/main/java/com/example/onepoint/KnowledgeAdapter.java
@@ -0,0 +1,6 @@
+package com.example.onepoint;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class KnowledgeAdapter extends RecyclerView.Adapter {
+}
