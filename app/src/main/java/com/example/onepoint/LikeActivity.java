66029e4d2573a1cf200d6e2cb984ce0430428cfb
ji-zhe <43602150+ji-zhe@users.noreply.github.com>
2020-05-17 19:17:48 +0800

    Update LikeActivity
    ActionBar and StatusBar

--- app/src/main/java/com/example/onepoint/LikeActivity.java
+++ app/src/main/java/com/example/onepoint/LikeActivity.java
@@ -8,9 +8,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridLayout;
 
@@ -48,6 +53,7 @@ public class LikeActivity extends AppCompatActivity {
                 finish();
             }
         });
+        setHalfTransparent();
         Button button_setting = (Button) findViewById(R.id.setting);
         button_setting.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,4 +79,75 @@ public class LikeActivity extends AppCompatActivity {
             knowledgeList.add(knowledges[index]);
         }
     }
+    /**
+     * 全透状态栏
+     */
+    protected void setStatusBarFullTransparent() {
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 半透明状态栏
+     */
+    protected void setHalfTransparent() {
+
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 如果需要内容紧贴着StatusBar
+     * 应该在对应的xml布局文件中，设置根布局fitsSystemWindows=true。
+     */
+    private View contentViewGroup;
+
+    protected void setFitSystemWindow(boolean fitSystemWindow) {
+        if (contentViewGroup == null) {
+            contentViewGroup = ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+        }
+        contentViewGroup.setFitsSystemWindows(fitSystemWindow);
+    }
+
+    /**
+     * 为了兼容4.4的抽屉布局->透明状态栏
+     */
+    /*protected void setDrawerLayoutFitSystemWindow() {
+        if (Build.VERSION.SDK_INT == 19) {//19表示4.4
+            int statusBarHeight = getStatusHeight(this);
+            if (contentViewGroup == null) {
+                contentViewGroup = ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+            }
+            if (contentViewGroup instanceof DrawerLayout) {
+                DrawerLayout drawerLayout = (DrawerLayout) contentViewGroup;
+                drawerLayout.setClipToPadding(true);
+                drawerLayout.setFitsSystemWindows(false);
+                for (int i = 0; i < drawerLayout.getChildCount(); i++) {
+                    View child = drawerLayout.getChildAt(i);
+                    child.setFitsSystemWindows(false);
+                    child.setPadding(0,statusBarHeight, 0, 0);
+                }
+
+            }
+        }
+    }*/
 }
--- app/src/main/java/com/example/onepoint/ReadActivity.java
+++ app/src/main/java/com/example/onepoint/ReadActivity.java
@@ -8,9 +8,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridLayout;
 
@@ -23,12 +28,12 @@ public class ReadActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
 
     private Knowledge[] knowledges = {
-            new Knowledge("figure 1: woeijfwq42iijfowijfoeqwe", R.drawable.fig1),
-            new Knowledge("figure 2", R.drawable.fig2),
-            new Knowledge("figure 3", R.drawable.fig3),
-            new Knowledge("figure 4", R.drawable.fig4),
-            new Knowledge("figure 5", R.drawable.fig5),
-            new Knowledge("figure 6", R.drawable.fig6)
+            new Knowledge("figure 1: 我看着你们，满怀羡慕", R.drawable.fig1),
+            new Knowledge("figure 2: 我们这一代的想象力不足以支撑你们的未来", R.drawable.fig2),
+            new Knowledge("figure 3: 你们把传统的变成现代的，把经典的变成流行的，把中国的变成世界的", R.drawable.fig3),
+            new Knowledge("figure 4: 睡觉吧！后浪！", R.drawable.fig4),
+            new Knowledge("figure 5: 他娘的爷终于做出来了！是谁杀了我，而我又杀了谁？", R.drawable.fig5),
+            new Knowledge("figure 6: 睡个好觉", R.drawable.fig6)
     };
 
     private List<Knowledge> knowledgeList = new ArrayList<>();
@@ -56,6 +61,7 @@ public class ReadActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        setHalfTransparent();
         initKnowledges();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
@@ -72,4 +78,53 @@ public class ReadActivity extends AppCompatActivity {
             knowledgeList.add(knowledges[index]);
         }
     }
+    /**
+     * 全透状态栏
+     */
+    protected void setStatusBarFullTransparent() {
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 半透明状态栏
+     */
+    protected void setHalfTransparent() {
+
+        if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        } else if (Build.VERSION.SDK_INT >= 19) {//19表示4.4
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 如果需要内容紧贴着StatusBar
+     * 应该在对应的xml布局文件中，设置根布局fitsSystemWindows=true。
+     */
+    private View contentViewGroup;
+
+    protected void setFitSystemWindow(boolean fitSystemWindow) {
+        if (contentViewGroup == null) {
+            contentViewGroup = ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+        }
+        contentViewGroup.setFitsSystemWindows(fitSystemWindow);
+    }
+
 }
