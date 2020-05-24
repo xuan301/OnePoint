052a797778469a9bd840b97ab32d4dbf95f46d25
Blackwatch <jinjipeng1@gmail.com>
2020-05-24 21:27:50 +0800

    添加了收藏按钮的转换和（不成熟的）左右滑动切换

--- app/src/main/java/com/example/onepoint/RandomKnowledgeActivity.java
+++ app/src/main/java/com/example/onepoint/RandomKnowledgeActivity.java
@@ -2,18 +2,104 @@ package com.example.onepoint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 public class RandomKnowledgeActivity extends AppCompatActivity {
+    private Button bt;
+    GestureDetector Detector;
+    private ScrollView content;
+    protected static final float FLIP_DISTANCE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_knowledge);
         getSupportActionBar().hide();
         setHalfTransparent();
+
+        bt=this.findViewById(R.id.like);
+
+        bt.setOnClickListener(new View.OnClickListener()
+        {//收藏按钮的切换
+            boolean isActive = false;
+            @Override
+            public void onClick(View view){
+                Drawable liked = getResources().getDrawable(R.drawable.ic_liked);
+                Drawable tolike = getResources().getDrawable(R.drawable.ic_like_black_24dp);
+
+                if(! isActive) {
+                    liked.setBounds(0,0,liked.getMinimumWidth(),liked.getMinimumHeight());
+                    bt.setCompoundDrawables(null, liked, null, null);
+                    bt.setText(getResources().getString(R.string.liked));
+                }
+                else{
+                    tolike.setBounds(0,0,tolike.getMinimumWidth(),tolike.getMinimumHeight());
+                    bt.setCompoundDrawables(null, tolike, null, null);
+                    bt.setText(getResources().getString(R.string.like));
+                }
+
+                isActive = ! isActive;
+            }
+        });
+
+        Detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if(e1.getX()-e2.getX()>FLIP_DISTANCE){
+                    Intent intent = new Intent(RandomKnowledgeActivity.this, RandomKnowledgeActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                else if(e2.getX()-e1.getX()>FLIP_DISTANCE){
+                    Intent intent = new Intent(RandomKnowledgeActivity.this, RandomKnowledgeActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return Detector.onTouchEvent(event);
     }
 
     protected void setHalfTransparent() {
