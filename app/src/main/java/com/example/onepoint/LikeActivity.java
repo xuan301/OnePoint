f4fff9b19b3e9085deeb3bf2207990937756f58e
Blackwatch <jinjipeng1@gmail.com>
2020-05-26 18:08:18 +0800

    将字符串写入xml

--- app/src/main/java/com/example/onepoint/LikeActivity.java
+++ app/src/main/java/com/example/onepoint/LikeActivity.java
@@ -37,11 +37,11 @@ public class LikeActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
 
     private Knowledge[] knowledges = {
-            new Knowledge("落霞与孤鹜齐飞，秋水共长天一色。——王勃《滕王阁序》", "https://wx2.sinaimg.cn/large/0067hdZuly1g7szjhsld7j30u01901kz.jpg"),
-            new Knowledge("【徐志摩！你的落款又傲娇又风骚】徐志摩在写给陆小曼的书信集《爱曼小札》中的落款很肉麻风骚", "https://wx3.sinaimg.cn/large/0067hdZugy1g7ssvg877pj30j60j675i.jpg"),
-            new Knowledge("晚安碎语【第十四话，私藏句子】我们常常不去想自己拥有的东西，却对得不到的东西念念不忘。——叔本华", "https://wx3.sinaimg.cn/large/0067hdZugy1g7ss38x252j30yi0yitau.jpg"),
-            new Knowledge("人走茶凉伤感吗？不伤感！真正伤感的是：人走了，却把我的茶杯也骗走了。——王小波《王小波文集》", "https://wx1.sinaimg.cn/large/0067hdZuly1g71vmsi9jqj30uv0u0tea.jpg"),
-            new Knowledge("所有感情都没有过分夸大的必要，因为没有什么只开始不结束。小时候觉得一辈子都不会弄丢的东西，最后也毫不心疼地丢弃了。亲情，爱情，友情，都会慢慢失去，有的自然而然，有的姿势难看。但感情的事，不就是这个样子吗。——姚瑶《生活上瘾指南》", "https://wx3.sinaimg.cn/large/0067hdZuly1g71efm9xpgj30u01hegy6.jpg")
+            new Knowledge("落霞与孤鹜齐飞，秋水共长天一色。——王勃《滕王阁序》", getString(R.string.fig1)),
+            new Knowledge("【徐志摩！你的落款又傲娇又风骚】徐志摩在写给陆小曼的书信集《爱曼小札》中的落款很肉麻风骚", getString(R.string.fig2)),
+            new Knowledge("晚安碎语【第十四话，私藏句子】我们常常不去想自己拥有的东西，却对得不到的东西念念不忘。——叔本华", getString(R.string.fig3)),
+            new Knowledge("人走茶凉伤感吗？不伤感！真正伤感的是：人走了，却把我的茶杯也骗走了。——王小波《王小波文集》", getString(R.string.fig4)),
+            new Knowledge("所有感情都没有过分夸大的必要，因为没有什么只开始不结束。小时候觉得一辈子都不会弄丢的东西，最后也毫不心疼地丢弃了。亲情，爱情，友情，都会慢慢失去，有的自然而然，有的姿势难看。但感情的事，不就是这个样子吗。——姚瑶《生活上瘾指南》", getString(R.string.fig5))
     };
 
     private List<Knowledge> knowledgeList = new ArrayList<>();
