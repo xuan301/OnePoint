87058d457433aee5ad75695721c1278f0418714e
wulala2580 <635033547@qq.com>
2020-06-13 19:16:36 +0800

    头像的简单替换

--- app/src/main/java/com/example/onepoint/InformationActivity.java
+++ app/src/main/java/com/example/onepoint/InformationActivity.java
@@ -1,8 +1,11 @@
 package com.example.onepoint;
 
+import android.annotation.SuppressLint;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -10,6 +13,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
@@ -113,8 +117,10 @@ public class InformationActivity extends AppCompatActivity implements View.OnCli
                 if(Build.VERSION.SDK_INT>=24){
                     imageUri = FileProvider.getUriForFile(InformationActivity.this,
                             "com.example.onepoint.fileprovider", outputImage);
+                    Log.e("imageUri SDK_INT >=24" ,imageUri.toString());
                 }else{
                     imageUri = Uri.fromFile(outputImage);
+                    Log.e("imageUri SDK_INT <24" ,imageUri.toString());
                 }
                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
@@ -148,12 +154,19 @@ public class InformationActivity extends AppCompatActivity implements View.OnCli
     private void crop(Uri uri) {
         // 裁剪图片意图
         Intent intent = new Intent("com.android.camera.action.CROP");
+/*        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            String url=getPath(getBaseContext(),uri);
+            intent.setDataAndType(Uri.fromFile(new File(url)), "image/*");
+        }else{
+            intent.setDataAndType(uri, "image/*");
+        }*/
+        intent.setDataAndType(uri, "image/*");
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             //添加这一句表示对目标应用临时授权该Uri所代表的文件
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         }
-        intent.setDataAndType(uri, "image/*");
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
 
@@ -185,6 +198,7 @@ public class InformationActivity extends AppCompatActivity implements View.OnCli
         //防止低版本没有返回确认数据，导致崩溃
         super.onActivityResult(requestCode, resultCode, data);
         System.out.println(requestCode);
+        System.out.println("result code: "+resultCode);
         if (requestCode == PHOTO_REQUEST_GALLERY) {
             System.out.println("相册返回");
             // 从相册返回的数据
@@ -193,7 +207,8 @@ public class InformationActivity extends AppCompatActivity implements View.OnCli
                 Uri uri = data.getData();
                 Bitmap bitmap = null;
                 try {
-                    bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                    System.out.println("imageuri: "+uri.toString());
+                    bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
                     iv_img.setImageBitmap(bitmap);
                     System.out.println("头像设置成功！");
                 } catch (FileNotFoundException e) {
@@ -223,6 +238,7 @@ public class InformationActivity extends AppCompatActivity implements View.OnCli
             // 从剪切图片返回的数据
             if (data != null) {
                 try{
+                    Log.e("request3", imageUri.toString());
                     Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
                     iv_img.setImageBitmap(bitmap);
                     //保存到SharedPreferences
@@ -303,6 +319,95 @@ public class InformationActivity extends AppCompatActivity implements View.OnCli
         }
 
     }
+    @SuppressLint("NewApi")
+    public static String getPath(final Context context, final Uri uri) {
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] { split[1] };
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
     protected void setHalfTransparent() {
 
         if (Build.VERSION.SDK_INT >= 21) {//21表示5.0
